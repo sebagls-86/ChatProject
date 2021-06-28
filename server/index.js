3b965e8b555b3a62404dd5653af8998d868589ec
Seba <sebastianglopezsosa@gmail.com>
2021-06-28 16:29:32 -0300

    cambios en home

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors')
+//const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
 app.use(function(req, res, next){
