685142d8878fe46ac835ff9005e82d1d631f4a8b
Seba <sebastianglopezsosa@gmail.com>
2021-06-28 17:16:45 -0300

    index

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(function(req, res, next){
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-type, Accept");
     next();
 
-})
+});
 
 const authRoutes = require('./routes/authRoutes');
 //app.use(cors(corsOptions));
